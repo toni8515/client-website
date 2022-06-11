80fbdb65f48c4cfe5db492691de84b64e238bace
Toni <jiaseeles@gmail.com>
2022-06-10 23:03:16 -0400

    A website for client

---
+++ resources/image_files/_0mQ_Y7dkdF.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js_script(document.currentScript); }/*FB_PKG_DELIM*/
+
+__d("LSPersistedDbGating",["MAWGating.re","gkx"],(function(a,b,c,d,e,f,g){"use strict";a=c("MAWGating.re").isLabyrinthOnWebEnabled()&&c("gkx")("6452");b=!a;d=b;g.onlyEncryptedBackupsPersisted=a;g.allTablesPersisted=b;g.oneMqttConnectionAcrossTabs=d}),98);
